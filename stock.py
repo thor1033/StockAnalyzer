4352283b42bc80fee882db6d9056ae9b9de39e14
Thor Bøje Simonsen <thor_simonsen@hotmail.com>
2020-04-30 18:22:37 +0200

    update Stock function

--- stock.py
+++ stock.py
@@ -1,15 +1,19 @@
+import pandas as pd
+import requests
+
 class Stock:
     
-    def __init__(self, name, ticker, price, maxPrice, minPrice):
-        self.name = name
-        self.ticker = ticker
-        self.currentPrice = price
-        self.maxPrice = maxPrice
-        self.minPrice = minPrice
+    def __init__(self, ticker):
+        return
+        #data = pd.read_json('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + ticker + '&apikey=BXAVNFY9YVG3DJDW')
+
 
     def updateStock(self, ticker):
-        print(ticker)
+        api_call = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="+ ticker + "&apikey=BXAVNFY9YVG3DJDW"
+        respond = requests.get(api_call)
+        data = pd.DataFrame.from_dict(respond.json()['Time Series (Daily)'])
+        print(data.head(10))
 
 if __name__ == '__main__':
-    stonk = Stock("Apple", "APPL", 2300, 2400, 2200)
-    stonk.updateStock('APPL')
+    stonk = Stock("IBM")
+    stonk.updateStock('AAPL')
