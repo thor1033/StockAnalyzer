eb5e1ac770e008d633900ecbadb21b8e9fac52f6
Thor Bøje Simonsen <thor_simonsen@hotmail.com>
2020-05-01 11:44:41 +0200

    added function VisualizeDailyCandles

--- stock.py
+++ stock.py
@@ -1,19 +1,40 @@
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 class Stock:
     
     def __init__(self, ticker):
-        return
-        #data = pd.read_json('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + ticker + '&apikey=BXAVNFY9YVG3DJDW')
+        self.ticker = ticker
 
 
-    def updateStock(self, ticker):
-        api_call = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="+ ticker + "&apikey=BXAVNFY9YVG3DJDW"
+    def updateStockDaily(self):
+        api_call = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="+ self.ticker + "&apikey=BXAVNFY9YVG3DJDW"
         respond = requests.get(api_call)
         data = pd.DataFrame.from_dict(respond.json()['Time Series (Daily)'])
-        print(data.head(10))
+        self.opens = data.iloc[0].values
+        self.highs = data.iloc[1].values
+        self.lows = data.iloc[2].values
+        self.closes = data.iloc[3].values
+        self.adjustedCloses = data.iloc[4].values
+        self.volumes = data.iloc[5].values
+        self.dividendAmount = data.iloc[6].values
+        self.splitCoefficients = data.iloc[7].values
+        self.dates = data.axes[1]
+
+    def visualizeDailyCandles(self):
+        fig = go.Figure(data=[go.Candlestick(x=self.dates,
+                        open = self.opens,
+                        high = self.highs,
+                        low = self.lows,
+                        close = self.closes)])
+        fig.show()
 
 if __name__ == '__main__':
     stonk = Stock("IBM")
-    stonk.updateStock('AAPL')
+    stonk.updateStockDaily()
+
+    stonk.visualizeDailyCandles()
+
+    print(stonk.opens)
